bf02b41284c6432d417af10c54005ba7186a0702
paul k <paul.kervegan@hotmail.fr>
2023-03-26 20:48:41 +0200

    progress on settingDesc

--- src/xmlanalysis.py
+++ src/xmlanalysis.py
@@ -1,7 +1,9 @@
 from zipfile import ZipFile
 from lxml import etree
 import requests
+import json
 import time
+import sys
 import re
 import os
 
@@ -21,8 +23,9 @@ import os
 # variables constantes: les chemins vers les fichiers/dossiers utilisés
 CURDIR = os.path.abspath(os.path.dirname(__file__))                              # dossier actuel
 TXT = os.path.abspath(os.path.join(CURDIR, os.pardir, "txt"))                    # dossier `txt/`
+WEB = os.path.abspath(os.path.join(CURDIR, os.pardir, "web"))                    # dossier `web/`
 XML = os.path.abspath(os.path.join(CURDIR, os.pardir, "xml"))                    # dossier `xml/`
-UNZIP = os.path.abspath(os.path.join(XML, "unzip"))
+UNZIP = os.path.abspath(os.path.join(XML, "unzip"))                              # dossier `xml/unzip`
 NS_TEI = {"tei": "http://www.tei-c.org/ns/1.0"}                                  # tei namespace
 NS_XML = {"id": "http://www.w3.org/XML/1998/namespace"}                          # general xml namespace
 TEI_RNG = "https://tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng"  # odd in .rng to validate tei files
@@ -43,17 +46,53 @@ def zip2tei():
 
 def setting_desc(corpus):
     """
-    créer un `settingDesc` dans le `profileDesc` du `teiHeader`
-    de chaque corpus. ce `settingDesc` contient une liste de tous
-    les lieux d'expédition/réception d'une lettre du corpus, avec
-    sa géolocalisation
+    faire des enrichissements géographiques du corpus:
+    - créer une liste de tous les lieux d'expédition/récéption du corpus
+    - à partir de cette liste, créer un `settingDesc` dans le 
+      `profileDesc` du `teiHeader` de chaque corpus. ce `settingDesc` 
+      contient une liste de tous les lieux, avec sa géolocalisation
+    - enregistrer la géolocalisation sous la forme d'un geojson 
+      dans le dossier `web/json`.
+    
+    api nominatim:
+    ~~~~~~~~~~~~~~
+    https://nominatim.org/release-docs/develop/api/Search/
+    
+    structure de sortie:
+    ~~~~~~~~~~~~~~~~~~~~
+    <settingDesc xmlns:tei="http://www.tei-c.org/ns/1.0">
+        <listPlace>
+            <place xml:id="kobe">
+                <placeName>Kobe</placeName>
+                <location>
+                    <geo>135.1943764 34.6932379</geo>
+                </location>
+            </place>
+            <!-- ... -->
+        </listPlace>
+    </settingDesc>
     
     :param corpus: la liste des fichiers xml du corpus
     :returns: la liste des fichiers xml du corpus, après une mise
               à jour de ces fichiers pour y ajouter le `settingDesc`
     """
     place_list = []  # liste de lieux
-    endpoint = "https://nominatim.openstreetmap.org/search?"
+    endpoint = "https://nominatim.openstreetmap.org/search?"  # url de l'api nominatim
+    
+    # on créé notre structure d'accueil pour les lieux:
+    # un élément tei settingDesc, qui contient un `listPlace`
+    # qui liste tout les lieux d'expédition/destination de notre
+    # corpus
+    settingDesc = etree.Element(
+        "settingDesc"
+        , nsmap=NS_TEI
+    )
+    listPlace = etree.SubElement(
+        settingDesc
+        , "listPlace"
+        , nsmap=NS_TEI
+    )
+    
     
     # d'abord, on construit une liste dédoublonnée de tous les différents
     # lieux d'expédition / récéption de lettres
@@ -71,14 +110,34 @@ def setting_desc(corpus):
     
     # ensuite, en utilisant l'API nominatim, récupérer 
     # les géolocalisations des différents lieux
-    for place in place_list:
-        if place == "NA":
-            continue  # ne pas traiter "NA", qui logiquement n'est pas géocoordonnée
+    for placename in place_list:
+        # créer un identifiant unique @xml:id
+        idx = "".join( c for c in placename.lower() if c.isalnum())
         
-        # on commence par chercher un nom de ville: la majorité des `place` sont des villes
+        # créer l'élément tei `place`, contenu par `listPlace`
+        # et qui contient toutes les informations sur le lieu
+        place = etree.SubElement(
+            listPlace
+            , "place"
+            , nsmap=NS_TEI
+        )
+        place.set("{http://www.w3.org/XML/1998/namespace}id", idx)
+        etree.SubElement(
+            place
+            , "placeName"
+            , nsmap=NS_TEI
+        ).text = placename
+        
+        if placename == "NA":
+            # "NA", logiquement, n'est pas géocoordonnée
+            # => on ne fait pas de requête sur nominatim et on
+            # ne complète pas l'élément `place` créé avec des infos géolocalisées 
+            continue
+        
+        # on commence par chercher un nom de ville: la majorité des `placename` sont des villes
         time.sleep(1)  # il faut attendre 1s entre 2 requêtes
         r = requests.get(endpoint, params={ 
-            "city": place,        # le nom de la ville recherchée
+            "city": placename,    # le nom de la ville recherchée
             "format": "geojson",  # format de la réponse: json
             "limit": 1            # nombre de résultats à afficher
         })
@@ -86,24 +145,47 @@ def setting_desc(corpus):
         # si on ne trouve pas de nom de ville, alors on fait une recherche 
         # libre en utilisant le param `q` à la place de `city`
         if len(r.json()) == 0:
-            print(place)
             time.sleep(1)
             r = requests.get(endpoint, params={ 
-                "q": place,           # `q`: query, paramètre de recherche libre
+                "q": placename,       # `q`: query, paramètre de recherche libre
                 "format": "geojson",  # format de la réponse: json
                 "limit": 1            # nombre de résultats à afficher
             })
         
         # si la requête s'est bien passée et qu'on a des résultats, alors
+        # - on complète notre élément xml `place` avec un `location` qui
+        #   contient un `geo`, celui-ci contenant les géocoordonnées
+        # - on enregristre notre geojson dans le dossier `WEB/json/`
         res = r.json()
         if r.status_code == 200 and len(res["features"]) > 0:
             
-            # on extrait les coordonnées
+            # on extrait les coordonnées et les ajoute à `place`
             if res["features"][0]["geometry"]["type"] == "Point":
-                coordinates = res["features"][0]["geometry"]["coordinates"]
+                # on convertit la liste de coordonnées en string
+                coordinates = "".join( 
+                    f"{c} " for c in res["features"][0]["geometry"]["coordinates"] 
+                ).strip()
+                # on crée notre élément xml
+                location = etree.SubElement(
+                    place
+                    , "location"
+                    , nsmap=NS_TEI
+                )
+                etree.SubElement(
+                    location
+                    , "geo"
+                    , nsmap=NS_TEI
+                ).text = coordinates
+                
+                # enfin, on enregistre notre json. la réponse est en geojson => on crée
+                # un fichier geojson et on l'enregistre, pour pouvoir y accéder +tard
+                with open( os.path.join(WEB, "json", f"{idx}.geojson"), mode="w") as fh:
+                    json.dump(res, fh, indent=4)
+                
             else:
-                print(res)
-            # print(r.json(), "\n\n")
+                # il n'y a pas de coordonnées, c'est étrange => on print
+                print(f"pas de coordonnées pour '{placename}'", "\n", res, "\n\n")
+    print(etree.tostring(settingDesc))
         
             
         
@@ -117,8 +199,11 @@ def pipeline():
     chaîne de traitement globale pour l'analyse du corpus en XML
     """
     # créer le dossier dans lequel les fichiers xml seront stockés
+    # ainsi que les fichiers de sortie
     if not os.path.isdir(UNZIP):
         os.makedirs(UNZIP)
+    if not os.path.isdir(os.path.join(WEB, "json")):
+        os.makedirs(os.path.join(WEB, "json"))
     
     # extraire tous les fichiers zippés
     corpus = zip2tei()
