f1def00c5ac008d30f788728f9b0fd60f7f082e6
paul k <paul.kervegan@hotmail.fr>
2023-03-28 03:28:33 +0200

    entites done

--- src/xmlanalysis.py
+++ src/xmlanalysis.py
@@ -1,6 +1,10 @@
+from langdetect import detect, detect_langs
+from collections import Counter
 from zipfile import ZipFile
 from lxml import etree
+import unidecode
 import requests
+import spacy
 import json
 import time
 import sys
@@ -45,9 +49,25 @@ def zip2tei():
     return [ os.path.join(UNZIP, f) for f in os.listdir(UNZIP) ]
 
 
+def xmlid(text):
+    """
+    fonction pour créer un @xml:id à partir de la chaîne de caractère `text`.
+    permet de normaliser la création d'identifiants uniques. on ne garde que
+    les caractères alphanumériques sans majuscules de `text` + on enlève les
+    accents des lettres avec `unidecode`.
+    
+    :param text: le texte à partir duquel produire un identifiant
+    :returns: l'identifiant
+    """
+    return unidecode.unidecode("".join( c for c in text.lower() if c.isalnum()))
+    
+
 def geography(corpus):
     """
-    faire des enrichissements géographiques du corpus:
+    faire des enrichissements géographiques du corpus
+    
+    chaine de traitement
+    ~~~~~~~~~~~~~~~~~~~~
     - créer une liste de tous les lieux d'expédition/récéption du corpus
     - à partir de cette liste, créer un `settingDesc` dans le 
       `profileDesc` du `teiHeader` de chaque corpus. ce `settingDesc` 
@@ -121,7 +141,7 @@ def geography(corpus):
     # des geojson de ces lieux
     for placename in place_list:
         # créer un identifiant unique @xml:id
-        idx = "".join( c for c in placename.lower() if c.isalnum())
+        idx = xmlid(placename)
         
         # créer l'élément tei `place`, contenu par `listPlace`
         # et qui contient toutes les informations sur le lieu
@@ -210,12 +230,12 @@ def geography(corpus):
         tree.xpath(".//tei:profileDesc", namespaces=NS_TEI)[0].append(settingDesc)
         
         # ajout des `@ref` aux `placeName`.
-        # pour ce faire, on simplifie juste le nom de lieu en supprimant espaces et 
-        # caractères non alphanumériques, ce qui revient à la manière dont on a créé
-        # les @xml:id de chaque lieu à partir du nom du lieu du `settingDesc`. 
+        # pour ce faire, on reconstruit un identifiant pour le lieu tel que l'`@xml:id`
+        # du lieu a été créé dans le `settingDesc` => il suffira de faire la comparaison.
+        # entre l'identifiant créé ici (`placetext`) et l'`@xml:id` du `settingDesc`  
         # on préfixe toujours la valeur d'un `@ref` par un `#`
         for placename in tree.xpath(".//*[not(tei:settingDesc)]//tei:placeName", namespaces=NS_TEI):
-            placetext = "".join( c for c in placename.text.lower() if c.isalnum() )
+            placetext = xmlid(placename.text)
             # inconnu, aucun => on donne la valeur `#na` à `@ref`
             if re.search("^(inconnu|aucun)$", placetext):
                 placename.set("ref", "#na")
@@ -241,10 +261,240 @@ def geography(corpus):
 def entity(corpus):
     """
     créer des enrichissements sur les personnes mentionnées
-    dans le corpus
+    dans le corpus. stocker toutes les données sur les entités
+    dans les corpus xml: `TEI//profileDesc/particDesc/(listPerson|listOrg)`
+    
+    chaîne de traitement
+    ~~~~~~~~~~~~~~~~~~~~
+    - on créé `entities`, un dictionnaire associant à l'identifiant 
+      unique de chaque expéditeur/destinataire les différentes orthographes 
+      de son nom + la langue dans laquelle la lettre comportant son nom
+      est écrite
+    - avec Spacy, on identifie le type de chaque entité nommée: est-ce que
+      c'est le nom d'une personne, d'une organisation? si oui, quel type 
+      d'organisation?
+    - on crée un `particDesc` qui contient un `listPerson` avec la liste
+      de `person` et un ;`listOrg` avec la liste d'`org` expéditrices/destinataires 
+      de lettres dans le corpus, en fonction des données produites par spacy
+    - on met à jour le `correspDesc` en fonction des informations dans
+      le `particDesc`
+    - on met à jour les fichiers
+    
+    à propos des librairies utilisées
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    spacy: documentation en bref: https://spacy.io/usage/spacy-101
+    spacy: ner: https://spacy.io/usage/linguistic-features#named-entities
+    langdetect: documentation: https://pypi.org/project/langdetect/
+    
+    installer spacy et les corpus de langues
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    :windows:
+    python -m venv .env.env\Scripts\activate
+    pip install -U pip setuptools wheel
+    pip install -U spacy
+    python -m spacy download en_core_web_trf
+    python -m spacy download fr_dep_news_trf
+    :linux/macos:
+    python -m venv .env
+    source .env/bin/activate
+    pip install -U pip setuptools wheel
+    pip install -U spacy
+    python -m spacy download en_core_web_trf
+    python -m spacy download fr_dep_news_trf
+    
     
     :return: corpus, la liste des chemins vers les fichiers
     """
+    # 1) DÉFINITION DES VARIABLES ET ÉLÉMENTS DE BASE
+    #
+    # entities associe à un xml:id 3 clés:
+    # - `name`: la liste des différentes orthographes de ce nom
+    # - `type`: le type d'entité (personne, organisation...)
+    # - `lang`: la langue du nom (pour définir le modèle spacy à utiliser)
+    entities = {}
+    lang_dict = {}                          # dictionnaire associant à l'xml:id d'une lettre la langue parlée                       
+    nlp_fr = spacy.load("en_core_web_trf")  # modèle spacy pour l'anglais
+    nlp_en = spacy.load("fr_dep_news_trf")  # modèle spacy pour le français
+    particDesc = etree.Element(             # élément xml qui accueillera la liste de personnes 
+        "particDesc"
+        , nsmap=NS_TEI
+    )
+    listPerson = etree.SubElement(          # la liste de personnes
+        particDesc
+        , "listPerson"
+        , nsmap=NS_TEI
+    )
+    listOrg = etree.SubElement(             # la liste des organisations
+        particDesc
+        , "listOrg"
+        , nsmap=NS_TEI
+    )
+    
+    # 1) PREMIER TRAITEMENT DES LETTRES
+    #
+    # 2 opérations:
+    # - on créé un `listPerson` uniquement en prenant
+    #   les nom des expéditeur.ice.s et des destinataires
+    #   dans les `correspAction` de chaque lettre. la méthode
+    #   de création est donc très similaire à celle du `listPlace`
+    #   dans la fonction `geography`
+    # - on détermine la langue de chaque lettre avec `langdetect` 
+    #   pour pouvoir déterminer le modèle spacy à utiliser
+    for fpath in corpus:
+        tree = etree.parse(fpath, parser=PARSER)
+        letter_idx = tree.xpath("./@xml:id", namespaces=NS_TEI)[0]  # letter's @xml:id
+        
+        # on détecte la langue de chaque lettre. pour ce faire, on extrait
+        # le texte de la lettre avec une xpath, on en fait une seule chaîne
+        # de caractère et on utilise `detect()` de la librairie `langdetect`
+        lang = detect( " ".join(s for s in tree.xpath(".//tei:body//text()", namespaces=NS_TEI)) )
+        lang_dict[letter_idx] = lang  # on ajoute la langue à `lang_dict`
+        
+        for pers in tree.xpath(".//tei:correspAction//tei:persName", namespaces=NS_TEI):
+            
+            # pour éviter les doublons inutiles, on ajoute pas tous les noms à `place_dict`
+            # - on enlève les initiales
+            # - on simplifie les chaînes de caractères avec la fonction `xmlid()`
+            # - on ajoute le nom non simplifié en valeur de `entities` seulement si
+            #   ce nom n'est pas déjà une valeur de `entities[idx]`
+            # - à chaque fois, on ajoute le code de la langue pour déterminer le modèle
+            #   à utiliser sur les noms extraits
+            idx = xmlid(re.sub("((?<=\s)|(?<=^))[A-Z][a-zàâäéèûüùîïì]*\.", "", pers.text))
+            
+            # traitement spécifique pour `na` et équivalents
+            if re.search("^(inconnu|aucun|na)$", idx):
+                if "na" not in entities.keys():
+                    entities["na"] = { "name": [pers.text], "type": "", "lang": [lang] }
+                elif pers.text not in entities["na"]["name"]:
+                    entities["na"]["name"].append(pers.text)
+                    entities["na"]["lang"].append(lang)
+            # pour les autres noms:
+            else:
+                if idx not in entities.keys():
+                    entities[idx] = { "name": [pers.text], "type": "", "lang": [lang] }
+                elif  pers.text not in entities[idx]["name"]:
+                    entities[idx]["name"].append(pers.text)
+                    entities[idx]["lang"].append(lang)
+    
+    
+    # 2) DÉTECTER LE TYPE D'ENTITÉ CONTENUE DANS `entities`
+    # on a donc un dictionnaire associant un @xml:id de nom à toutes 
+    # ses orthographes et aux langues des lettres où le nom apparaît
+    # ensuite, on détecte le type d'entité nommée contenue dans `entities`
+    for k in entities.keys():
+        entities[k]["lang"] = Counter(entities[k]["lang"]).most_common(1)[0][0]  # on extrait le mode, soit la langue la + utilisée pour les lettres avec ce nom
+        for name in entities[k]["name"]:
+            # on sélectionne le bon modèle en fonction du langage. 
+            # si le langage détecté n'est ni le français, ni l'anglais,
+            # on affiche une erreur mais on utilise le modèle français: la
+            # lettre est surement en français 
+            if entities[k]["lang"] == "fr":
+                doc = nlp_fr(name)
+            elif entities[k]["lang"] == "en":
+                doc = nlp_en(name)
+            else:
+                print(f"pas de modèle disponible pour la langue '{entities[k]['lang']}'"
+                      + f"détectée dans la lettre: '{letter_idx}'. utilisation du modèle"
+                      + " français par défaut 'fr_dep_news_trf'")
+                doc = nlp_fr(name)
+            
+            # enfin, avec spacy, on détecte le type d'entité nommée encodée dans
+            # les `correspAction` et on ajoute le label donné à l'entité nommée par spacy 
+            # dans `name` à `entities`. spacy produit plusieurs entités nommées => on extrait
+            # le mode, soit le type d'entité le + souvent détecté dans notre `name`. vu la 
+            # taille de notre corpus c'est un peu inutile, mais bon
+            type_ = Counter([ ent.label_ for ent in doc.ents ]).most_common(1)
+            entities[k]["type"] = type_[0][0] if len(type_) > 0 else ""
+            # pour une explication de chaque entité nommée identifiée dans `name`,
+            # décocher le bloc ci-dessous
+            # for ent in doc.ents: 
+            #     print(name
+            #           , "~~~~~~~~~~~~~~~~"
+            #           , ent.text
+            #           , ent.label_
+            #           , spacy.explain(ent.label_))
+
+    # 3) CRÉER LE `PARTICDESC`
+    #
+    # on a extrait toutes nos entités nommées. il ne reste plus qu'à créer notre 
+    # `particDesc`. celui-ci contiendra:
+    # -  un `listPerson`, avec tous les membres du `correspAction` identifié.es
+    #    comme des personnes; 
+    # - un `listOrg`, avec tous les membres du `correspAction` qui n'ont pas été 
+    #   identifié.es comme des personnes par spacy.
+    # pour chaque `person` / `org`, ajouter un `@xml:id` (la clé de `entities`)
+    # et un `@type` (qui contient le type d'entité identifié par spacy
+    for k in entities.keys():
+        # définir `entity`, un élément de `listPerson` ou de `listOrg`.
+        # déterminer son élément conteneur `parent` (`listPerson` ou `listOrg`)
+        # , le nom de l'élément lui-même `el` (`person` / `org`)
+        # , le nom du `elname`: `persName` ou `orgName`, élément indiquant le nom de `el`
+        if entities[k]["type"] == "PERSON" or entities[k]["type"] == "":
+            parent = listPerson
+            el = "person"
+            elname = "persName"
+        else:
+            parent = listOrg
+            el = "org"
+            elname = "orgName"
+        # définir `entity`
+        entity = etree.SubElement(
+            parent
+            , el
+            , nsmap=NS_TEI
+        )
+        if entities[k]["type"] != "":
+            entity.set("type", entities[k]["type"])  # définir le `@type`: type d'entité
+        entity.set("{http://www.w3.org/XML/1998/namespace}id", k)  # définir l'identifiant de `entity`
+        # définir ses sous-éléments `persName` ou `orgName`, portant les 
+        # variantes d'appellations associées à cette entité dans les 
+        # différentes lettres du corpus
+        for name in entities[k]["name"]:
+            etree.SubElement(
+                entity
+                , elname
+                , nsmap=NS_TEI
+            ).text = name
+            
+    # 4) METTRE À JOUR LES FICHIERS XML
+    #
+    # on reparse les fichiers xml pour:
+    # - modifier les `correspAction`: si le `persName` renvoie en fait
+    #   à une organisation, alors remplacer ce `persName` par un `orgName`
+    #   (on fait ça à l'aide des différents `persName` / `orgName` dans le
+    #   `particDesc`
+    # - ajouter les `@ref` dans le `correspAction` qui font référence aux 
+    #   @xml:id des entités identifiées dans le `particDesc`
+    # - ajouter le `particDesc` au `encodingDesc` du fichier xml
+    # - sauvegarder les arbres mis à jour
+    for fpath in corpus:
+        tree = etree.parse(fpath, parser=PARSER)
+        
+        # ajout du `particDesc` à l'arbre
+        tree.xpath(".//tei:profileDesc", namespaces=NS_TEI)[0].append(particDesc)
+        
+        # on modifie les correspAction:
+        # `corresp` = tous les noms expéditeur.ice.s/destinataires dans le `correspAction`
+        for corresp in tree.xpath(".//tei:correspAction/tei:persName", namespaces=NS_TEI):
+            
+            # on cible l'élément du `particDesc` qui correspond à `corresp`
+            for matched_entity in particDesc.xpath(f".//*[./text()='{corresp.text}']", namespaces=NS_TEI):
+                # on met à jour le tag de `corresp`: si avec spacy, on a détecté que le nom
+                # dans `corresp` n'est pas celui d'une personne, alors on change `persName` par
+                # le bon tag
+                corresp.tag = matched_entity.tag
+                # enfin, on ajoute un `@ref` à `corresp` qui pointe vers le bon
+                # `persName / org` dans le `particDesc` 
+                idx = matched_entity.xpath("./parent::*/@xml:id")[0]
+                corresp.set("ref", f"#{idx}")
+        
+        # on sauvegarde le fichier
+        etree.cleanup_namespaces(tree)
+        etree.ElementTree(tree.getroot()).write(
+            fpath
+            , pretty_print=True
+        )
+    
     return corpus
 
 
@@ -260,14 +510,17 @@ def pipeline():
         os.makedirs(os.path.join(WEB, "json"))
     
     # extraire tous les fichiers zippés
+    print("dézippage des fichiers XML")
     corpus = zip2tei()
     
     # créer le `settingDesc` dans le `profileDesc` 
     # + produire des informations spatiales pour chaque lettre
+    print("enrichissement géographique des fichiers")
     corpus = geography(corpus)
     
     # créer le `particDesc` + faire de la reconnaissance
     # d'entités nommées pour chaque lettre du corpus
+    print("traitement des noms de personnes et d'organisations")
     corpus = entity(corpus)
     
     return
